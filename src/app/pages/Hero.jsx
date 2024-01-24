5f5f589dc3fc5a021d2ab3647c392b30f2c518a2
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-01-24 15:15:09 +0100

    hero text refactor to make less messy

--- src/app/pages/Hero.jsx
+++ src/app/pages/Hero.jsx
@@ -8,23 +8,25 @@ export default function Hero() {
 				<h1 className='hero__title'>
 					<span className='hero--greeting'>{`Kia ora, I'm...`}</span>
 					<span className='hero--name'> Chris Wagg</span>
-					{`I try and
-					build cool, accessibile stuff for the internet`}
 				</h1>
 				<p className='hero__text'>
 					Welcome to my website.
-					<span>
+					<span className='hero__highlight'>
 						{` I try and build cool, accessibile stuff for the internet`}
 					</span>
-					{` Back in 2021 I found myself between work and decided to try
-					my hand at learning how to code and here we are. I'm a front
-					end developer who's passionate about`}
-					<span className='hero__highlight'> UI</span>,
-					<span className='hero__highlight'> accessibility </span>
-					and <span className='hero__highlight'> teaching</span>.
-					{`I
-					love building things that look good, are responsive, and
-					that everyone can use. Maybe you might like some of it too!`}
+					<span>
+						{` Back in 2021 I found myself  between work and decided to try
+						my hand at learning how to code and here we are. I'm a front
+						end developer who's passionate about`}
+					</span>
+					<span className='hero__highlight'>
+						{` UI, accessibility and teaching.`}
+					</span>
+					<span>
+						{` I
+						love building things that look good, are responsive, and
+						that everyone can use. Maybe you might like some of it too!`}
+					</span>
 				</p>
 			</section>
 		</>
