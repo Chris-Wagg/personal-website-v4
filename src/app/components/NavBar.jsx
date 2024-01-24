e096a8f3fcc025d5922393b351999be33df1ad9e
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-01-24 14:05:04 +0100

    nav bar and hero skeleton added

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -5,32 +5,49 @@ export default function NavBar() {
 		<header className='.head'>
 			<div className='head__logo'>Chris Wagg</div>
 			<nav className='nav-bar'>
-				<button className='nav-bar__btb'>
-					About me <Link aria-hidden='true' href='#about-me'></Link>
-				</button>
+				<Link href='#about-me'>
+					<button className='nav-bar__btn'>About me</button>
+				</Link>
+				<Link href='#Projects'>
+					<button className='nav-bar__btn'>Projects</button>
+				</Link>
+				<Link href='#Experience'>
+					<button className='nav-bar__btn'>Experience</button>
+				</Link>
+				<Link href='https://github.com/Chris-Wagg'>
+					<button aria-label='Github' className='nav-bar__btn'>
+						<img
+							className='nav-bar__img-svg'
+							src='./github-mark.svg'
+							alt='Github'
+						/>
+					</button>
+				</Link>
+				<Link href='https://www.linkedin.com/in/chriswagg'>
+					<button aria-label='Linkedin' className='nav-bar__btn'>
+						<img
+							className='nav-bar__img-svg'
+							src='./linkedin.svg'
+							alt='Linkedin'
+						/>
+					</button>
+				</Link>
+				<Link href='https://www.frontendmentor.io/profile/Chris-Wagg'>
+					<button
+						aria-label='Frontend Mentor'
+						className='nav-bar__btn'>
+						<img
+							className='nav-bar__img-svg'
+							src=''
+							alt='Frontend Mentor'
+						/>
+					</button>
+				</Link>
 
-				<button className='nav-bar__btb'>
-					Projects
-					<Link aria-hidden='true' href='#Projects' />
-				</button>
-
-				<button className='nav-bar__btb'>
-					Experience
-					<Link aria-hidden='true' href='#Experience' />
-				</button>
-
-				<button aria-label='Github' className='nav-bar__btb'>
-					<img src='./github-mark.svg' alt='' />
-					<Link aria-hidden='true' href='#Github' />
-				</button>
-
-				<button aria-label='Linkedin' className='nav-bar__btb'>
-					<img src='./linkedin.svg' alt='' />
-					<Link aria-hidden='true' href='#Linkedin' />
-				</button>
-
-				<button className='nav-bar__btb'>
-					Resume <Link aria-hidden='true' href='#Resume' />
+				<button className='nav-bar__btn'>
+					<a href='./Chris-Wagg-CV.pdf' download>
+						Resume
+					</a>
 				</button>
 			</nav>
 		</header>
--- src/app/pages/Hero.jsx
+++ src/app/pages/Hero.jsx
@@ -4,7 +4,27 @@ export default function Hero() {
 	return (
 		<>
 			<NavBar />
-			<section className='hero'>my name jeff</section>
+			<section className='hero'>
+				<h1 className='hero__title'>
+					<span className='hero--greeting'>Kia ora, I'm...</span>
+					<span className='hero--name'> Chris Wagg</span> I try and
+					build cool, accessibile stuff for the internet
+				</h1>
+				<p className='hero__text'>
+					Welcome to my website.
+					<span>
+						I try and build cool, accessibile stuff for the internet
+					</span>
+					Back in 2021 I found myself between work and decided to try
+					my hand at learning how to code and here we are. I'm a front
+					end developer who's passionate about
+					<span className='hero__highlight'> UI</span>,
+					<span className='hero__highlight'> accessibility </span>
+					and <span className='hero__highlight'> teaching</span>. I
+					love building things that look good, are responsive, and
+					that everyone can use. Maybe you might like some of it too!
+				</p>
+			</section>
 		</>
 	)
 }
