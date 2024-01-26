d8420cbfb3a805dab9d6e3f8119bef0a432625f1
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-01-26 14:10:52 +0100

    projects cards added

---
+++ src/app/components/ProjectCard.jsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function ProjectCard(props) {
+	const { projectName, builtWith, liveLink, githubLink, cardImage, cardAlt } =
+		props
+
+	return (
+		<div className='card'>
+			<h3 className='card__title'>{projectName}</h3>
+			<img src={`./${cardImage}.jpg`} alt={cardAlt} />
+			<p className='card__built-with'>{builtWith}</p>
+			<a href={liveLink} className='card__live-link'>
+				Check it out!
+			</a>
+			<a href={githubLink}>
+				<Image
+					className='card__github-svg'
+					src='./github-mark.svg'
+					alt='Github'
+					width={30}
+					height={30}
+				/>
+			</a>
+		</div>
+	)
+}
--- src/app/pages/Projects.jsx
+++ src/app/pages/Projects.jsx
@@ -1,8 +1,25 @@
+import ProjectCard from '../components/ProjectCard'
+
 export default function Projects() {
 	return (
 		<section className='projects'>
 			<h2 className='projects__title'>Projects</h2>
-			<div className='projects__card'></div>
+			<ProjectCard
+				projectName='Middle Earth Travel'
+				cardImage='ME-travel-screenshot'
+				cardAlt='Middle Earth travel screenshot'
+				projectTech='Next.js, HTML, CSS'
+				liveLink='https://chris-wagg.github.io/travel-v2/'
+				githubLink='https://github.com/Chris-Wagg/travel-v2'
+			/>
+			<ProjectCard
+				projectName='Frontend Mentor Projects'
+				cardImage='FEM-screenshot'
+				cardAlt='Frontend Mentor screenshot'
+				projectTech='Next.js, HTML, CSS, JS'
+				liveLink='https://www.frontendmentor.io/profile/Chris-Wagg'
+				githubLink='https://github.com/orgs/Chris-Wagg-Frontend-Mentor-Projects/repositories'
+			/>
 		</section>
 	)
 }
