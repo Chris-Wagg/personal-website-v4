a98d1f6d76d8924d97f9680dfd365e4fee30a172
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-01-27 14:04:21 +0100

    footer added

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,3 +1,40 @@
 export default function Footer() {
-	return <footer className='footer'></footer>
+	return (
+		<footer className='footer'>
+			<div className='contaxt-text'>
+				<h2>Contact me!</h2>
+				<p>
+					{`Want to drop me a line? Have some questions? Just feel like
+					a chat? Send me a message and I'll get back to you as soon
+					as I can.`}
+				</p>
+			</div>
+			<form action='submit' className='contact-form'>
+				<input
+					type='text'
+					id='name'
+					name='name'
+					placeholder='Name'
+					className='contact-form__input'
+				/>
+
+				<input
+					type='email'
+					id='email'
+					name='email'
+					placeholder='Email'
+					className='contact-form__input'
+				/>
+
+				<textarea
+					name='message'
+					id='message'
+					cols='30'
+					rows='10'
+					className='contact-form__input--text-area'
+				/>
+				<button>Send message</button>
+			</form>
+		</footer>
+	)
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Hero from './pages/Hero'
 import Experience from './pages/Experience'
 import Projects from './pages/Projects'
+import Footer from './components/Footer'
 
 export default function Home() {
 	return (
@@ -8,6 +9,7 @@ export default function Home() {
 			<Hero />
 			<Experience />
 			<Projects />
+			<Footer />
 		</>
 	)
 }
