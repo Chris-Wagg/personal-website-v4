d961b1454ee89631a457d01f15e3b33c9a24282e
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-02-28 12:44:03 +0100

    nav buttons changed to links and animations added

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -7,50 +7,49 @@ export default function NavBar() {
 			<div className='head-wrapper'>
 				<h1 className='head__logo'>Chris Wagg</h1>
 				<nav className='nav-bar'>
-					<Link href='#about-me'>
-						<button className='nav-bar__btn'>About me</button>
+					<Link href='#about-me' className='nav-bar__btn'>
+						About me
 					</Link>
-					<Link href='#Projects'>
-						<button className='nav-bar__btn'>Projects</button>
+					<Link href='#Projects' className='nav-bar__btn'>
+						Projects
 					</Link>
-					<Link href='#Experience'>
-						<button className='nav-bar__btn'>Experience</button>
+					<Link href='#Experience' className='nav-bar__btn'>
+						experience
 					</Link>
-					<Link href='https://github.com/Chris-Wagg'>
-						<button aria-label='Github' className='nav-bar__btn'>
-							<Image
-								className='nav-bar__img-svg'
-								src='./github-mark-white.svg'
-								alt='Github'
-								width={30}
-								height={30}
-							/>
-						</button>
+					<Link
+						href='https://github.com/Chris-Wagg'
+						className='nav-bar__btn'>
+						<Image
+							className='nav-bar__img-svg'
+							src='./github-mark-white.svg'
+							alt='Github'
+							width={30}
+							height={30}
+						/>
 					</Link>
-					<Link href='https://www.linkedin.com/in/chriswagg'>
-						<button aria-label='Linkedin' className='nav-bar__btn'>
-							<Image
-								className='nav-bar__img-svg'
-								src='./linkedin.svg'
-								alt='Linkedin'
-								width={30}
-								height={30}
-							/>
-						</button>
+					<Link
+						href='https://www.linkedin.com/in/chriswagg'
+						className='nav-bar__btn'>
+						<Image
+							className='nav-bar__img-svg'
+							src='./linkedin.svg'
+							alt='Linkedin'
+							width={30}
+							height={30}
+						/>
 					</Link>
-					<Link href='https://www.frontendmentor.io/profile/Chris-Wagg'>
-						<button
-							aria-label='Frontend Mentor'
-							className='nav-bar__btn'>
-							<Image
-								className='nav-bar__img-svg'
-								src=''
-								alt='Frontend Mentor'
-								width={30}
-								height={30}
-							/>
-						</button>
+					<Link
+						href='https://www.frontendmentor.io/profile/Chris-Wagg'
+						className='nav-bar__btn'>
+						<Image
+							className='nav-bar__img-svg'
+							src=''
+							alt='Frontend Mentor'
+							width={30}
+							height={30}
+						/>
 					</Link>
+
 					<button className='nav-bar__btn'>
 						<a href='./Chris-Wagg-CV.pdf' download>
 							Resume
