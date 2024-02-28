97f7c2925d700da18babfd6ab7661f340953a677
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-02-28 13:57:09 +0100

    hero styles added

--- src/app/pages/Hero.jsx
+++ src/app/pages/Hero.jsx
@@ -5,29 +5,31 @@ export default function Hero() {
 		<>
 			<NavBar />
 			<section className='hero'>
-				<h1 className='hero__title'>
-					<span className='hero--greeting'>{`Kia ora, I'm...`}</span>
-					<span className='hero--name'> Chris Wagg</span>
-				</h1>
-				<p className='hero__text'>
-					Welcome to my website.
-					<span className='hero__highlight'>
-						{` I try and build cool, accessibile stuff for the internet`}
-					</span>
-					<span>
-						{` Back in 2021 I found myself  between work and decided to try
-						my hand at learning how to code and here we are. I'm a front
-						end developer who's passionate about`}
-					</span>
-					<span className='hero__highlight'>
-						{` UI, accessibility and teaching.`}
-					</span>
-					<span>
-						{` I
-						love building things that look good, are responsive, and
-						that everyone can use. Maybe you might like some of it too!`}
-					</span>
-				</p>
+				<div>
+					<h1 className='hero__title'>
+						<span className='hero--greeting'>{`Kia ora, I'm...`}</span>
+						<span className='hero--name'> Chris Wagg</span>
+					</h1>
+					<p className='hero__text'>
+						Welcome to my website.
+						<span className='hero__highlight'>
+							{` I try and build cool, accessibile stuff for the internet`}
+						</span>
+						<span>
+							{` Back in 2021 I found myself  between work and decided to try
+							my hand at learning how to code and here we are. I'm a front
+							end developer who's passionate about`}
+						</span>
+						<span className='hero__highlight'>
+							{` UI, accessibility and teaching.`}
+						</span>
+						<span>
+							{` I
+							love building things that look good, are responsive, and
+							that everyone can use. Maybe you might like some of it too!`}
+						</span>
+					</p>
+				</div>
 			</section>
 		</>
 	)
