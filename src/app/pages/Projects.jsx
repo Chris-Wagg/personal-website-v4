fa9596668f495383334f154219eb14d20ec3252c
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-02-29 13:22:18 +0100

    projects styles and minor css refactor

--- src/app/pages/Projects.jsx
+++ src/app/pages/Projects.jsx
@@ -4,22 +4,24 @@ export default function Projects() {
 	return (
 		<section className='projects'>
 			<h2 className='projects__title'>Projects</h2>
-			<ProjectCard
-				projectName='Middle Earth Travel'
-				cardImage='ME-travel-screenshot'
-				cardAlt='Middle Earth travel screenshot'
-				projectTech='Next.js, HTML, CSS'
-				liveLink='https://chris-wagg.github.io/travel-v2/'
-				githubLink='https://github.com/Chris-Wagg/travel-v2'
-			/>
-			<ProjectCard
-				projectName='Frontend Mentor Projects'
-				cardImage='FEM-screenshot'
-				cardAlt='Frontend Mentor screenshot'
-				projectTech='Next.js, HTML, CSS, JS'
-				liveLink='https://www.frontendmentor.io/profile/Chris-Wagg'
-				githubLink='https://github.com/orgs/Chris-Wagg-Frontend-Mentor-Projects/repositories'
-			/>
+			<div className='projects__wrapper'>
+				<ProjectCard
+					projectName='Middle Earth Travel'
+					cardImage='ME-travel-screenshot'
+					cardAlt='Middle Earth travel screenshot'
+					projectTech='Next.js, HTML, CSS'
+					liveLink='https://chris-wagg.github.io/travel-v2/'
+					githubLink='https://github.com/Chris-Wagg/travel-v2'
+				/>
+				<ProjectCard
+					projectName='Frontend Mentor Projects'
+					cardImage='FEM-screenshot'
+					cardAlt='Frontend Mentor screenshot'
+					projectTech='Next.js, HTML, CSS, JS'
+					liveLink='https://www.frontendmentor.io/profile/Chris-Wagg'
+					githubLink='https://github.com/orgs/Chris-Wagg-Frontend-Mentor-Projects/repositories'
+				/>
+			</div>
 		</section>
 	)
 }
