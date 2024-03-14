1d75bd53cd3770921e5f692d8dc3cc17329f28b9
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-03-14 14:04:25 +0100

    svh attribution added

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,12 +2,19 @@ export default function Footer() {
 	return (
 		<footer className='footer'>
 			<div className='contact-text'>
-				<h2>Contact me!</h2>
-				<p>
-					{`Want to drop me a line? Have some questions? Just feel like
-					a chat? Send me a message and I'll get back to you as soon
-					as I can.`}
-				</p>
+				<div>
+					<h2>Contact me!</h2>
+					<p>
+						{`Want to drop me a line? Have some questions? Just feel like
+						a chat? Send me a message and I'll get back to you as soon
+						as I can.`}
+					</p>
+				</div>
+				<a
+					className='attibution'
+					href='https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/'>
+					Free SVG Backgrounds and Patterns by SVGBackgrounds.com
+				</a>
 			</div>
 			<form action='submit' className='contact-form'>
 				<input
