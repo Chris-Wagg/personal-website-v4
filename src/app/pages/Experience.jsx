a9ef38864bde5871cc7e0a843329e08bbed55dfd
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-03-15 13:38:17 +0100

    experience layout changed to gridlayout with flex on the items

--- src/app/pages/Experience.jsx
+++ src/app/pages/Experience.jsx
@@ -1,25 +1,27 @@
 export default function Experience() {
 	return (
-		<section className='experience'>
-			<div className='card'>
-				<h3 className='card__title'>HTML</h3>
-				<p className='card__text'>2+ years</p>
-			</div>
-			<div className='card'>
-				<h3 className='card__title'>CSS</h3>
-				<p className='card__text'>2+ years</p>
-			</div>
-			<div className='card'>
-				<h3 className='card__title'>Javascript</h3>
-				<p className='card__text'>2+ years</p>
-			</div>
-			<div className='card'>
-				<h3 className='card__title'>React/ Next.js</h3>
-				<p className='card__text'>2+ years</p>
-			</div>
-			<div className='card'>
-				<h3 className='card__title'>Accessibility</h3>
-				<p className='card__text'>{`< 1 year`}</p>
+		<section className='experience content-grid'>
+			<div className='experience-card-wrapper breakout-full'>
+				<div className='card'>
+					<h3 className='card__title'>HTML</h3>
+					<p className='card__text'>2+ years</p>
+				</div>
+				<div className='card'>
+					<h3 className='card__title'>CSS</h3>
+					<p className='card__text'>2+ years</p>
+				</div>
+				<div className='card'>
+					<h3 className='card__title'>Javascript</h3>
+					<p className='card__text'>2+ years</p>
+				</div>
+				<div className='card'>
+					<h3 className='card__title'>React/ Next.js</h3>
+					<p className='card__text'>2+ years</p>
+				</div>
+				<div className='card'>
+					<h3 className='card__title'>Accessibility</h3>
+					<p className='card__text'>{`< 1 year`}</p>
+				</div>
 			</div>
 		</section>
 	)
