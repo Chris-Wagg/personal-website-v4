8bcc4504d87044b851a08e69d6e08db5e12108f6
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-03-21 12:25:06 +0100

    fixed footer spacings

--- src/app/pages/Hero.jsx
+++ src/app/pages/Hero.jsx
@@ -30,13 +30,9 @@ export default function Hero() {
 									that everyone can use. Maybe you might like some of it too!`}
 							</span>
 						</p>
-						<Link
-							href='#contact'
-							// scroll={false}
-							className='contact-btn'>
+						<Link href='#contact' className='contact-btn'>
 							Get in touch!
 						</Link>
-						{/* TODO: fix scroll to send message */}
 					</div>
 				</div>
 			</section>
