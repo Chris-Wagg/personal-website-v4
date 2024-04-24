b93228d0757d5ac6630521f53fb5895d56c29170
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-04-24 12:55:05 +0200

    hero spelling fix

--- src/app/pages/Hero.jsx
+++ src/app/pages/Hero.jsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 export default function Hero() {
 	return (
 		<>
-			{/* <NavBar /> */}
 			<section className='hero '>
 				<div className='hero__wrapper content-grid'>
 					<h1 className='hero__title'>
@@ -14,19 +13,17 @@ export default function Hero() {
 						<p className='hero__text '>
 							Welcome to my website.
 							<span className='hero__highlight'>
-								{` I try and build cool, accessibile stuff for the internet.`}
+								{` I try and build cool, accessible stuff for the internet.`}
 							</span>
 							<span>
-								{` Back in 2021 I found myself  between work and decided to try
-									my hand at learning how to code and here we are. I'm a front
+								{` Back in 2021 I found myself  between work and decided to try my hand at learning how to code and here we are. I'm a front
 									end developer who's passionate about`}
 							</span>
 							<span className='hero__highlight'>
 								{` UI, accessibility and teaching.`}
 							</span>
 							<span>
-								{` I
-									love building things that look good, are responsive, and
+								{` I love building things that look good, are responsive, and
 									that everyone can use. Maybe you might like some of it too!`}
 							</span>
 						</p>
